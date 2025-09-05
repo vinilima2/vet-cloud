9bced7b491db5004b0596322d34ab5becf356ed0
Thomas Pinheiro <thomashenriquesantospinheiro@gmail.com>
2025-09-05 07:56:19 -0300

    Update dashboard.tsx

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,11 +1,25 @@
 import { AppSidebar } from "~/components/app-side-bar";
 import { SidebarProvider, SidebarTrigger } from "~/components/ui/sidebar";
+import { adicionarPet } from "~/services/pet-service";
+import { Button } from "~/components/ui/button";
 
 export default function Dashboard() {
     return (
-        <SidebarProvider>
+        /*<SidebarProvider>
             <AppSidebar />
             <SidebarTrigger />
-        </SidebarProvider>
+        </SidebarProvider> */
+        <Button onClick={() => {
+            adicionarPet({
+            nome: "Zé",
+            anoNascimento: 2020,
+            especie: "Cachorro",
+            raca: "Basset Hound",
+            sexo: "M",
+            cor: "preto",
+            peso: 8.5,
+            ativo: true,
+            })  
+        }}>Adicionar Pet</Button>
     )
 }
