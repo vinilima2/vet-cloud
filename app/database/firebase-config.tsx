5db00da8fd14313e152e4d271464f2ed5dc0c918
Vinicius Lima <vinicius.limavl001@gmail.com>
2025-09-20 21:38:36 -0300

    fix: removendo chave antiga.

--- app/database/firebase-config.tsx
+++ app/database/firebase-config.tsx
@@ -7,13 +7,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCh-xwvQOD0rI0LKgIPyF2FpWaXx5dicKI",
-  authDomain: "vue-course-50638.firebaseapp.com",
-  databaseURL: "https://vue-course-50638-default-rtdb.firebaseio.com",
-  projectId: "vue-course-50638",
-  storageBucket: "vue-course-50638.firebasestorage.app",
-  messagingSenderId: "127793290426",
-  appId: "1:127793290426:web:2cc03be9046329a875f683"
+
 };
 
 // Initialize Firebase
